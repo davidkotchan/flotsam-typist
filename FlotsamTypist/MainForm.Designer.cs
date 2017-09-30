ce8bdb07725ac59bb8e8d87a8087b069ec1cf9c1
David Kotchan <dkotchan@hotmail.com>
2017-09-30 13:52:50 -0700

    Initial commit.

---
+++ FlotsamTypist/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FlotsamTypist
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextOverlay = new WinFormsControls.TransparentLabel();
+            this.TypewriterPhrase = new WinFormsControls.TransparentLabel();
+            this.Underliner = new WinFormsControls.TransparentLabel();
+            this.SuspendLayout();
+            // 
+            // TextOverlay
+            // 
+            this.TextOverlay.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextOverlay.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.TextOverlay.Location = new System.Drawing.Point(28, 32);
+            this.TextOverlay.Name = "TextOverlay";
+            this.TextOverlay.Size = new System.Drawing.Size(220, 32);
+            this.TextOverlay.TabIndex = 2;
+            this.TextOverlay.TabStop = false;
+            this.TextOverlay.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // TypewriterPhrase
+            // 
+            this.TypewriterPhrase.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TypewriterPhrase.ForeColor = System.Drawing.Color.Khaki;
+            this.TypewriterPhrase.Location = new System.Drawing.Point(28, 32);
+            this.TypewriterPhrase.Name = "TypewriterPhrase";
+            this.TypewriterPhrase.Size = new System.Drawing.Size(220, 32);
+            this.TypewriterPhrase.TabIndex = 0;
+            this.TypewriterPhrase.TabStop = false;
+            this.TypewriterPhrase.Text = "YMMMMMMMMMMMMY";
+            this.TypewriterPhrase.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            // 
+            // Underliner
+            // 
+            this.Underliner.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Underliner.ForeColor = System.Drawing.Color.Crimson;
+            this.Underliner.Location = new System.Drawing.Point(28, 34);
+            this.Underliner.Name = "Underliner";
+            this.Underliner.Size = new System.Drawing.Size(220, 41);
+            this.Underliner.TabIndex = 1;
+            this.Underliner.TabStop = false;
+            this.Underliner.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(276, 104);
+            this.Controls.Add(this.TextOverlay);
+            this.Controls.Add(this.TypewriterPhrase);
+            this.Controls.Add(this.Underliner);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flotsam Typist";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WinFormsControls.TransparentLabel TypewriterPhrase;
+        private WinFormsControls.TransparentLabel Underliner;
+        private WinFormsControls.TransparentLabel TextOverlay;
+    }
+}
+
---
+++ FlotsamTypist/MainForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FlotsamTypist
+{
+    public partial class MainForm : Form
+    {
+        public const string LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string NUMBERS = "1234567890";
+        public const string PUNCTUATION = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+
+        public string FullTypewriterPhraseText { get; set; }
+
+        public const string UNDERBAR = "_";
+
+        public readonly int NumLetters;
+        public readonly int NumNumbers;
+        public readonly int NumPuncts;
+
+        // rand.Next( 5, 10 ) ==> 5, 6, 7, 8, 9 at random; not "10", 10 is exclusive upper bound.
+        // rand.Next( 5 ) ==> 0, 1, 2, 3, 4 at random, similarly.
+        public Random rand = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            NumLetters = LETTERS.Length;
+            NumNumbers = NUMBERS.Length;
+            NumPuncts = PUNCTUATION.Length;
+
+            DoAnotherPhrase();
+        }
+
+        private void DoAnotherPhrase()
+        {
+            int numCharsAvailable = NumLetters + NumNumbers + NumPuncts;
+
+            FullTypewriterPhraseText = "";
+
+            // int howLongTypewriterPhrase = rand.Next( 5, 11 );
+            int howLongTypewriterPhrase = 14;
+
+            for (int j = 0; j < howLongTypewriterPhrase; ++j)
+            {
+                int randCharIndex = rand.Next( 0, numCharsAvailable );
+                char randChar = (LETTERS + NUMBERS + PUNCTUATION)[ randCharIndex ];
+
+                FullTypewriterPhraseText += randChar;
+            }
+
+            TextOverlay.Text = "";      // no text
+            TypewriterPhrase.Text = FullTypewriterPhraseText;
+
+            SetUnderbarPosition();
+        }
+
+        private void ResetAndStartOver()
+        {
+            // Pause, give the user time to savor her success.
+            Thread.Sleep( 200 );
+
+            DoAnotherPhrase();
+        }
+
+        private void SetUnderbarPosition()
+        {
+            // Repeating number of characters in a string.  See:
+            //
+            // http://stackoverflow.com/questions/4115064/is-there-a-built-in-function-to-repeat-string-or-char-in-net
+            //
+            Underliner.Text = string.Join( "", Enumerable.Repeat( ' ', TextOverlay.Text.Length )) + UNDERBAR;
+        }
+
+        private void MainForm_KeyPress( object sender, KeyPressEventArgs e )
+        {
+            switch (e.KeyChar)
+            {
+                case (char) Keys.Escape:
+                    e.Handled = true;
+                    this.Close();       // close the Form, end the application
+                    break;
+
+                case (char) Keys.Back:  // Backspace
+                    e.Handled = true;
+                    AdvanceCorrectCharacter( -1 );
+                    break;
+
+                default:
+                    e.Handled = true;
+                    if (IsCorrectNextKey( e.KeyChar ))
+                    {
+                        bool anyCharsRemaining = AdvanceCorrectCharacter();
+                        if (!anyCharsRemaining)
+                        {
+                            ResetAndStartOver();
+                        }
+                    }
+                    else
+                    {
+                        FlashFormBackground( 100 );
+                    }
+                    break;
+            }
+        }
+
+        private bool IsCorrectNextKey( int keyChar )
+        {
+            int currentPosn = TextOverlay.Text.Length;
+
+            int correctKey = (int) TypewriterPhrase.Text[ currentPosn ];
+
+            bool isCorrectKey = (keyChar == correctKey);
+
+            return isCorrectKey;
+        }
+
+        private void FlashFormBackground( int milliseconds )
+        {
+            // Change the UI...
+            string originalTextOverlayText = TextOverlay.Text;
+            string originalTypewritePhraseText = TypewriterPhrase.Text;
+
+            string fullText = originalTextOverlayText + originalTypewritePhraseText.Trim();
+
+            TextOverlay.Text = "";    // no text
+            TypewriterPhrase.Text = fullText;
+
+            Color originalTypewritePhraseColor = TypewriterPhrase.ForeColor;
+            TypewriterPhrase.ForeColor = Color.Black;
+
+            Color originalFormBackgroundColor = this.BackColor;
+
+            BackColor = Color.White;
+
+            // ... and immediately update the screen to show it.  See:
+            //
+            // http://stackoverflow.com/questions/1360944/force-gui-update-from-ui-thread
+            //
+            Invalidate();
+            Update();
+            Refresh();
+            Application.DoEvents();     // necessary! or the TransparentLabel's don't redraw.
+
+            // Pause, give the user time to see and ponder the color update.
+            Thread.Sleep( milliseconds );
+
+            // Then revert back to the original background color.
+            TextOverlay.Text = originalTextOverlayText;
+            TypewriterPhrase.Text = originalTypewritePhraseText;
+            TypewriterPhrase.ForeColor = originalTypewritePhraseColor;
+
+            BackColor = originalFormBackgroundColor;
+        }
+
+        private bool AdvanceCorrectCharacter( int numCharsToAdvance = 1 )
+        {
+            int numCorrectChars = TextOverlay.Text.Length + numCharsToAdvance;
+
+            // Lower bound check.
+            if (numCorrectChars < 0)
+            {
+                numCorrectChars = 0;
+            }
+
+            // Upper bound check.
+            if (numCorrectChars > FullTypewriterPhraseText.Length)
+            {
+                numCorrectChars = FullTypewriterPhraseText.Length;
+            }
+
+            TextOverlay.Text = FullTypewriterPhraseText.Substring( 0, numCorrectChars );
+
+            // Repeating number of characters in a string.  See:
+            //
+            // http://stackoverflow.com/questions/4115064/is-there-a-built-in-function-to-repeat-string-or-char-in-net
+            //
+            TypewriterPhrase.Text = string.Join( "", Enumerable.Repeat( ' ', TextOverlay.Text.Length )) 
+                + FullTypewriterPhraseText.Substring( numCorrectChars, FullTypewriterPhraseText.Length - numCorrectChars );
+            
+            SetUnderbarPosition();
+
+            bool anyCharsRemaining = (Underliner.Text.Length <= FullTypewriterPhraseText.Length);
+            return anyCharsRemaining;
+        }
+    }
+}
